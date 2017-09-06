e76e31fcdd99699ce7b406d9d2328c4c9d4af406
emonhaider <habib.haider@gmail.com>
2017-09-06 00:16:20 -0400

    added navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  
+  public openSidebar(){                
+    console.log('i am here');    
+  }
 }
